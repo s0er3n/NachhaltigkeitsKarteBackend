ff55e095283801f649c4d1d0cf2c2c989e9df359
Sören Michaels <soeren.michaels@gmail.com>
2021-01-30 14:20:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+from tinydb import TinyDB, Query
+from typing import Optional
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+db = TinyDB('db.json')
+app = FastAPI()
+origins = [
+  "https://nachhaltigkeitskarte.netlify.app",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+class Location(BaseModel):
+    name: str
+    website: str
+    info: str
+    category: str
+    reason: str
+    image: str
+    googlemaps: str
+
+
+@app.get("/")
+def main():
+  return db.all()
+
+@app.post("/addLocation/")
+def addLocation(location: Location):
+    data = location.dict()
+    data["lat"], data["lon"] = data["googlemaps"].split(
+                "@")[1].split(",")[0:2]
+    
+    data["category"] = [c.strip()
+                        for c in data["category"].split(",") if c.strip() != ""]
+    data["approved"] = False
+    db.insert(data) 
+    return location
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, log_level="info")
