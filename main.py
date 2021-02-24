f9c64c3010bbda2a0ddce544114816ec6610374f
s0er3n <48496757+s0er3n@users.noreply.github.com>
2021-02-24 13:17:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from typing import Optional
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
 db = TinyDB('db.json')
 app = FastAPI()
 
